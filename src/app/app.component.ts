50d753a684ab8d32794250a557b5f3253195e1da
ellyssatan <ellyssa.tan1807@gmail.com>
2023-02-03 23:12:02 +0800

    shopping cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'day33-shoppingcart-prac';
+  
+  user !: User
+  name !: string
+
+  processNewOrder(u: User) {
+    this
+    console.info(">>> From App.component ", this.user)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './components/items.component';
+import { OrderItemsComponent } from './components/order-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    OrderItemsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
